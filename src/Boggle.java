b86798f79dd305104926b4335ea23bf582b4b70c
kpi <kieran.pichai@menloschool.org>
2025-01-13 22:18:32 -0800

    asdas d

--- src/Boggle.java
+++ src/Boggle.java
@@ -1,19 +1,61 @@
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class Boggle {
+    private static HashSet<String> goodWords;
+    private static Trie dict;
 
     public static String[] findWords(char[][] board, String[] dictionary) {
+        goodWords = new HashSet<>();
+        dict = new Trie();
+        initializeTrie(dictionary);
 
-        ArrayList<String> goodWords = new ArrayList<String>();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs(i, j, "", board);
+            }
+        }
 
-        // TODO: Complete the function findWords(). Add all words that are found both on the board
-        //  and in the dictionary.
-
-        // Convert the list into a sorted array of strings, then return the array.
+        // Convert the list into a sorted array of strings, then return the array
         String[] sol = new String[goodWords.size()];
         goodWords.toArray(sol);
         Arrays.sort(sol);
         return sol;
+    }
+
+    private static void initializeTrie(String[] dictionary) {
+        for (String s : dictionary) {
+            dict.insert(s);
+        }
+    }
+
+    public static boolean prefixExists(String prefix) {
+        return dict.inTrie(prefix, true);
+    }
+
+    public static void searchWordInDict(String word) {
+        if (dict.inTrie(word, false)) {
+            goodWords.add(word);
+        }
+    }
+
+    public static void dfs(int row, int col, String prefix, char[][] board) {
+        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length) return;
+        if (board[row][col] == 'K') return;
+        if (!prefixExists(prefix)) return;
+
+        // Mark this square as visited
+        char temp = board[row][col];
+        prefix += board[row][col];
+        board[row][col] = 'K';
+
+        searchWordInDict(prefix);
+
+        dfs(row - 1, col, prefix, board);
+        dfs(row + 1, col, prefix, board);
+        dfs(row, col - 1, prefix, board);
+        dfs(row, col + 1, prefix, board);
+
+        board[row][col] = temp;
     }
 }
---
+++ src/Trie.java
@@ -0,0 +1,44 @@
+public class Trie {
+    private Node root;
+
+    // Constructor for Trie
+    public Trie() {
+        // Stores a root node as instance variable
+        this.root = new Node(false);
+    }
+
+    // Insert method that takes in a string and inserts it into the trie
+    public void insert(String insertString) {
+        // Loops through string (similar to going down layers in trie)
+        Node curPos = root;
+        for (int i = 0; i < insertString.length(); i++) {
+            // Checks if our next layer is null
+            if (curPos.getNextSet()[insertString.charAt(i) - 'a'] == null) {
+                curPos.getNextSet()[insertString.charAt(i) - 'a'] = new Node(false);
+            }
+            // Increments down a layer
+            curPos = curPos.getNextSet()[insertString.charAt(i) - 'a'];
+        }
+        curPos.setWordEnd(true);
+    }
+
+    // inTrie is a search algorithm that searches for a given string in a trie
+    public boolean inTrie(String searchFor, boolean searchingForPrefix) {
+        Node curPos = root;
+        // Loops through indices in string and checks if each exists
+        for (int i = 0; i < searchFor.length(); i++) {
+            curPos = curPos.getNextSet()[searchFor.charAt(i) - 'a'];
+            // Early exit condition
+            if (curPos == null) {
+                return false;
+            }
+        }
+        // If it gets through the whole string checks if final position is end of word
+        return searchingForPrefix || curPos.getWordEnd();
+    }
+
+    // Getter for root node
+    public Node getRoot() {
+        return root;
+    }
+}
